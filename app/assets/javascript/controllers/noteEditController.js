8141191dc06ef25ada6f9e9ac1b3402e0e2a9180
Joe Tufo <joe.tufo@velir.com>
2016-04-21 09:37:02 -0400

    Finish note edit function

--- app/assets/javascript/controllers/noteEditController.js
+++ app/assets/javascript/controllers/noteEditController.js
@@ -1,10 +1,17 @@
 // Doesn't need [ ] because we're already assigning to the module in app.js
-angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, $routeParams) {
+angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, $routeParams, $location) {
 	$scope.note = Note.get({id: $routeParams.id});
 	$scope.isSubmitting = false;
 
 	$scope.saveNote = function(note) {
+		// Set isSumbitting to true on click
 		$scope.isSubmitting = true;
-		note.$update();
+
+		// When note is updated and promise is finished, reset isSubmitting back to false
+		note.$update().finally(function() {
+			$scope.isSubmitting = false;
+			// Redirct back to note using $location service
+			$location.path('notes/' + note.id);
+		});
 	}
 });
