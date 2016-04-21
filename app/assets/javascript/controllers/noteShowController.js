e4d02fc6591cf0f27a6b29e65ca8fe7f18ff6ac4
Joe Tufo <joe.tufo@velir.com>
2016-04-20 22:48:35 -0400

    Show notes and link them

--- app/assets/javascript/controllers/noteShowController.js
+++ app/assets/javascript/controllers/noteShowController.js
@@ -1,4 +1,8 @@
 // Doesn't need [ ] because we're already assigning to the module in app.js
-angular.module('NoteWrangler').controller('NotesShowController', function() {
+angular.module('NoteWrangler').controller('NotesShowController', function(Note, $scope, $routeParams) {
+	// routeParams gives us access to these wild cards :id
+	// routeParams.id is accessing the :id in the show route
+	$scope.note = Note.get({id: $routeParams.id});
 
+	console.log($scope.note);
 });
