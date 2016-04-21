c1d85b1565c06618f4598254f64e389e818c7b93
Joe Tufo <joe.tufo@velir.com>
2016-04-21 09:47:04 -0400

    Add Delete note functionality

--- app/assets/javascript/controllers/noteShowController.js
+++ app/assets/javascript/controllers/noteShowController.js
@@ -1,6 +1,13 @@
 // Doesn't need [ ] because we're already assigning to the module in app.js
-angular.module('NoteWrangler').controller('NotesShowController', function(Note, $scope, $routeParams) {
+angular.module('NoteWrangler').controller('NotesShowController', function(Note, $scope, $routeParams, $location) {
 	// routeParams gives us access to these wild cards :id
 	// routeParams.id is accessing the :id in the show route
 	$scope.note = Note.get({id: $routeParams.id});
+
+	$scope.deleteNote = function(note) {
+		// Use then, if it fails, it won't delete it
+		note.$remove().then(function() {
+			$location.path('/notes');
+		});
+	}
 });
