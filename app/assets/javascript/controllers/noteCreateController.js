890c4619cbdc2e08806330e6cbc944d73b6b6226
Joe Tufo <joe.tufo@velir.com>
2016-04-21 10:13:30 -0400

    New Note functionality

--- app/assets/javascript/controllers/noteCreateController.js
+++ app/assets/javascript/controllers/noteCreateController.js
@@ -1,4 +1,18 @@
 // Doesn't need [ ] because we're already assigning to the module in app.js
-angular.module('NoteWrangler').controller('NotesCreateController', function() {
+angular.module('NoteWrangler').controller('NotesCreateController', function($scope, Note, $location) {
+	// Because note doesn't exist, create new note
+	$scope.note = new Note();
+	$scope.isSubmitting = false;
 
+	$scope.saveNote = function(note) {
+		$scope.isSubmitting = true;
+
+		// then is successful
+		note.$save().then(function() {
+			$location.path('/notes');
+			// finally when finished
+		}).finally(function() {
+			$scope.isSubmitting = false;
+		});
+	};
 });
